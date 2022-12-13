15b0421d63eb221a9e02d0ed2edfe370349821df
Veera Phani <veeraphani21@gmail.com>
2022-12-13 22:03:28 +0530

    third commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,22 +3,22 @@ package com.driver;
 public class Main {
 public static class Product{
 
-    public int Product(int x,int y){
+    public int product(int x,int y){
         return x*y;
 
     }
-    public int Product(int x,int y,int z){
+    public int product(int x,int y,int z){
         return x*y*z;
     }
-    public double Product(double x,double y){
+    public double product(double x,double y){
         return x*y;
     }
 }
 
     public static void main(String[] args) {
     Product p = new Product();
-    p.Product(6,8);
-    p.Product(8,9,10);
-    p.Product(8.5,9.75);
+    p.product(6,8);
+    p.product(8,9,10);
+    p.product(8.5,9.75);
     }
 }
