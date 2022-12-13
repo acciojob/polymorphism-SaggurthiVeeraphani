b8cf6dca65b8ecfb533f38f74b1757c057b1062e
Veera Phani <veeraphani21@gmail.com>
2022-12-13 20:24:39 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+public static class Product{
 
+    public int Product(int x,int y){
+        return x+y;
+
+    }
+    public int Product(int x,int y,int z){
+        return x+y+z;
+    }
+    public double Product(double x,double y){
+        return x+y;
+    }
+}
+
+    public static void main(String[] args) {
+    Product p = new Product();
+    p.Product(6,8);
+    p.Product(8,9,10);
+    p.Product(8.5,9.75);
+    }
 }
